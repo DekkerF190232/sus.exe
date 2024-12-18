23b718e37c9bee02268d8ec9059e8a7b1219b879
Sirvierl0ffel <iminternetimmeranonym@gmail.com>
2024-12-18 05:11:00 +0100

    struct ctr, read strct member

--- asm/compile.cmd
+++ asm/compile.cmd
@@ -2,7 +2,7 @@
 cls
 prompt ^>$S
 :: prompt $s
-set path=C:\NASM\;%path%
+::set path=C:\NASM\;%path%
 set path=C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.40.33807\bin\Hostx64\x86\;%path%
 @echo on
 
